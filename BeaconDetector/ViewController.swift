979c2026c05a70a20f0d1a728498dbfcb2ab3b55
alperen23230 <alperenunal68@gmail.com>
2019-06-28 10:01:12 +0300

    notification added

--- BeaconDetector/ViewController.swift
+++ BeaconDetector/ViewController.swift
@@ -8,37 +8,95 @@
 
 import UIKit
 import CoreLocation
+import UserNotifications
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, UNUserNotificationCenterDelegate {
 
     @IBOutlet var distanceReading: UILabel!
-    var locationManager: CLLocationManager?
+    let locationManager = CLLocationManager()
+    
+    var beaconRegion: CLBeaconRegion!
     override func viewDidLoad() {
         super.viewDidLoad()
-        locationManager = CLLocationManager()
-        locationManager?.delegate = self
-        locationManager?.requestAlwaysAuthorization()
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            print("Permission granted? \(granted)")
+        }
+        UNUserNotificationCenter.current().delegate = self
+        
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        
+        let uuid = UUID(uuidString: "6fd8c37b-ebf6-43ae-b2eb-343e9c2730c4")!
+        beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 3755, minor: 10926, identifier: uuid.uuidString)
+        
+        if CLLocationManager.authorizationStatus() != .authorizedAlways {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            locationManager.startMonitoring(for: beaconRegion)
+        }
         
         view.backgroundColor = .gray
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if status == .authorizedAlways{
-            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self){
-                if CLLocationManager.isRangingAvailable(){
-                    startScanning()
-                }
+        switch status {
+        case .authorizedAlways:
+            if !locationManager.monitoredRegions.contains(beaconRegion) {
+                locationManager.startMonitoring(for: beaconRegion)
             }
+        case .authorizedWhenInUse:
+            if !locationManager.monitoredRegions.contains(beaconRegion) {
+                locationManager.startMonitoring(for: beaconRegion)
+            }
+        default:
+            print("authorisation not granted")
         }
     }
     
-    func startScanning(){
-        let uuid = UUID(uuidString: "6fd8c37b-ebf6-43ae-b2eb-343e9c2730c4")!
-        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 3755, minor: 0, identifier: "MyBeacon")
-        locationManager?.startMonitoring(for: beaconRegion)
-        locationManager?.startRangingBeacons(in: beaconRegion)
-    }
+//    func startScanning(){
+//        let uuid = UUID(uuidString: "6fd8c37b-ebf6-43ae-b2eb-343e9c2730c4")!
+//        beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 3755, minor: 10926, identifier: "MyBeacon")
+//        locationManager.startMonitoring(for: beaconRegion)
+//        locationManager.startRangingBeacons(in: beaconRegion)
+//    }
 
+    
+    func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion) {
+        print("Did determine state for region \(region)")
+        if state == .inside {
+            locationManager.startRangingBeacons(in: beaconRegion)
+            postNotification()
+        } else {
+            print("outside")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
+        print("Did start monitoring region: \(region)\n")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        locationManager.startRangingBeacons(in: beaconRegion)
+        print("didEnter")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        locationManager.stopRangingBeacons(in: beaconRegion)
+        print("didExit")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if beacons.count > 0 {
+            update(distance: beacons[0].proximity)
+            print(beacons[0].accuracy)
+            print(beacons[0].rssi)
+            
+        } else {
+            update(distance: .unknown)
+        }
+    }
+    
     
     func update(distance: CLProximity){
         UIView.animate(withDuration: 0.8){
@@ -64,18 +122,24 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
-    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
-        if beacons.count > 0 {
-           update(distance: beacons[0].proximity)
-            print(beacons[0].accuracy)
-            print(beacons[0].rssi)
-            
-        } else {
-            update(distance: .unknown)
+    
+    func postNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Beacon Detected!"
+        content.body = "Enter Area!"
+        content.sound = UNNotificationSound.default
+        let request = UNNotificationRequest(identifier: "EntryNotification", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            }
         }
     }
-    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
-        print(region)
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler(.alert)
     }
+    
+    
 }
 
